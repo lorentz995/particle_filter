0887b3555e0c84a0fda5a2ef1596570fa71c10e0
lorentz995 <lorenzonaticchi@yahoo.it>
2021-04-16 18:54:56 +0200

    Moved Class Particle in utils, commented unused code

--- src/particle_filter/particelle.py
+++ src/particle_filter/particelle.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-""" This is the starter code for the robot localization project """
-
 import rospy
 from octomap_msgs.srv import GetOctomap
 
@@ -17,7 +15,7 @@ from tf import TransformListener
 from tf import TransformBroadcaster
 from tf.transformations import euler_from_quaternion, rotation_matrix, quaternion_from_matrix
 from random import gauss
-from particle_filter.utils import OccupancyField
+from particle_filter.utils import Particle
 from nav_msgs.msg import OccupancyGrid
 
 import math
@@ -28,32 +26,10 @@ import numpy as np
 from numpy.random import random_sample, normal
 
 
-def convert_pose_to_xy_and_theta(pose):
-    """ Convert pose (geometry_msgs.Pose) to a (x,y,yaw) tuple """
-    orientation_tuple = (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
-    angles = euler_from_quaternion(orientation_tuple)
-    return pose.position.x, pose.position.y, angles[2]
-
-
-class Particle(object):
-    def __init__(self, x=0.0, y=0.0, theta=0.0, w=1.0):
-        self.w = w
-        self.theta = theta
-        self.x = x
-        self.y = y
-
-    def as_pose(self):
-        """ A helper function to convert a particle to a geometry_msgs/Pose message """
-        orientation_tuple = tf.transformations.quaternion_from_euler(0, 0, self.theta)
-        return Pose(position=Point(x=self.x, y=self.y, z=0),
-                    orientation=Quaternion(x=orientation_tuple[0], y=orientation_tuple[1], z=orientation_tuple[2],
-                                           w=orientation_tuple[3]))
-
-
 class ParticleFilter:
     def __init__(self):
         self.initialized = False
-        rospy.init_node('SoLillo')
+        rospy.init_node('Particle_Filter')
         self.base_frame = "base_link"  # the frame of the robot base
         self.map_frame = "map"  # the name of the map coordinate
         self.odom_frame = "odom"
@@ -63,16 +39,8 @@ class ParticleFilter:
 
         self.particle_cloud = []
 
-        self.current_odom_xy_theta = []
+        # self.current_odom_xy_theta = []
 
-        '''
-        # request the map from the map server, the map should be of type nav_msgs/OccupancyGrid
-        
-        # self.map_server = rospy.ServiceProxy('octomap_full', GetOctomap)
-        # self.map = self.map_server().map
-        # for now we have commented out the occupancy field initialization until you can successfully fetch the map
-
-        # rospy.spin() '''
         self.initialized = True
 
     def publish_particles(self, msg):
@@ -101,22 +69,19 @@ class ParticleFilter:
             radius_max = random.random() * rad_max
             # x => straight ahead
             x = radius_min * math.sin(other_theta) + (
-                    (map.info.width - map.info.origin.position.x) * map.info.resolution) / 2  # origine.x
+                    (map.info.width - map.info.origin.position.x) * map.info.resolution) / 2
             y = radius_max * math.cos(other_theta) + (
-                    (map.info.height - map.info.origin.position.y) * map.info.resolution) / 2  # origine.y
+                    (map.info.height - map.info.origin.position.y) * map.info.resolution) / 2
             particle = Particle(x, y, theta)
             self.particle_cloud.append(particle)
         p.publish_particles(self.particle_cloud)
 
         return self.particle_cloud
 
-        # self.normalize_particles()
-        # self.update_robot_pose()
-
 
 def load_map(map):
     # occupancy_field = OccupancyField(map)
-    print("seconda callback")
+    print("second callback")
 
 
 if __name__ == '__main__':
--- src/particle_filter/utils.py
+++ src/particle_filter/utils.py
@@ -22,6 +22,22 @@ import numpy as np
 from numpy.random import random_sample
 from sklearn.neighbors import NearestNeighbors
 
+
+class Particle(object):
+    def __init__(self, x=0.0, y=0.0, theta=0.0, w=1.0):
+        self.w = w
+        self.theta = theta
+        self.x = x
+        self.y = y
+
+    def as_pose(self):
+        """ A helper function to convert a particle to a geometry_msgs/Pose message """
+        orientation_tuple = tf.transformations.quaternion_from_euler(0, 0, self.theta)
+        return Pose(position=Point(x=self.x, y=self.y, z=0),
+                    orientation=Quaternion(x=orientation_tuple[0], y=orientation_tuple[1], z=orientation_tuple[2],
+                                           w=orientation_tuple[3]))
+
+
 class OccupancyField(object):
     """ Stores an occupancy field for an input map.  An occupancy field returns the distance to the closest
         obstacle for any coordinate in the map
@@ -31,9 +47,10 @@ class OccupancyField(object):
     """
 
     def __init__(self, map):
-        self.map = map      # save this for later
+        self.map = map  # save this for later
         # build up a numpy array of the coordinates of each grid cell in the map
-        X = np.zeros((self.map.info.width*self.map.info.height, 2)) # qui c'era self.map.info.width*self.map.info.height, 2
+        X = np.zeros(
+            (self.map.info.width * self.map.info.height, 2))  # qui c'era self.map.info.width*self.map.info.height, 2
 
         # while we're at it let's count the number of occupied cells
         total_occupied = 0
@@ -41,7 +58,7 @@ class OccupancyField(object):
         for i in range(self.map.info.width):
             for j in range(self.map.info.height):
                 # occupancy grids are stored in row major order, if you go through this right, you might be able to use curr
-                ind = i + j*self.map.info.width
+                ind = i + j * self.map.info.width
                 if self.map.data[ind] > 0:
                     total_occupied += 1
                 X[curr, 0] = float(i)
@@ -49,35 +66,34 @@ class OccupancyField(object):
                 curr += 1
 
         # build up a numpy array of the coordinates of each occupied grid cell in the map
-        O = np.zeros((total_occupied,2))
+        O = np.zeros((total_occupied, 2))
         curr = 0
         for i in range(self.map.info.width):
             for j in range(self.map.info.height):
                 # occupancy grids are stored in row major order, if you go through this right, you might be able to use curr
-                ind = i + j*self.map.info.width
+                ind = i + j * self.map.info.width
                 if self.map.data[ind] > 0:
-                    O[curr,0] = float(i)
-                    O[curr,1] = float(j)
+                    O[curr, 0] = float(i)
+                    O[curr, 1] = float(j)
                     curr += 1
 
         # use super fast scikit learn nearest neighbor algorithm
-        nbrs = NearestNeighbors(n_neighbors=1,algorithm="ball_tree").fit(O)
+        nbrs = NearestNeighbors(n_neighbors=1, algorithm="ball_tree").fit(O)
         distances, indices = nbrs.kneighbors(X)
 
         self.closest_occ = {}
         curr = 0
         for i in range(self.map.info.width):
             for j in range(self.map.info.height):
-                ind = i + j*self.map.info.width
-                self.closest_occ[ind] = distances[curr][0]*self.map.info.height
+                ind = i + j * self.map.info.width
+                self.closest_occ[ind] = distances[curr][0] * self.map.info.height
                 curr += 1
 
-
-    def get_closest_obstacle_distance(self,x,y):
+    def get_closest_obstacle_distance(self, x, y):
         """ Compute the closest obstacle to the specified (x,y) coordinate in the map.  If the (x,y) coordinate
             is out of the map boundaries, nan will be returned. """
-        x_coord = int((x - self.map.origin.position.x/self.map.info.resolution))
-        y_coord = int((y - self.map.origin.position.y/self.map.info.resolution))
+        x_coord = int((x - self.map.origin.position.x / self.map.info.resolution))
+        y_coord = int((y - self.map.origin.position.y / self.map.info.resolution))
 
         # check if we are in bounds
         if x_coord > self.map.info.width or x_coord < 0:
@@ -85,7 +101,14 @@ class OccupancyField(object):
         if y_coord > self.map.info.height or y_coord < 0:
             return float('nan')
 
-        ind = x_coord + y_coord*self.map.info.width
-        if ind >= self.map.info.width*self.map.info.height or ind < 0:
+        ind = x_coord + y_coord * self.map.info.width
+        if ind >= self.map.info.width * self.map.info.height or ind < 0:
             return float('nan')
         return self.closest_occ[ind]
+
+
+'''def convert_pose_to_xy_and_theta(pose):
+    """ Convert pose (geometry_msgs.Pose) to a (x,y,yaw) tuple """
+    orientation_tuple = (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
+    angles = euler_from_quaternion(orientation_tuple)
+    return pose.position.x, pose.position.y, angles[2]'''
