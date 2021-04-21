c8adb0855410d991d6354403902764dc134aaef3
lorentz995 <lorenzonaticchi@yahoo.it>
2021-04-21 16:28:55 +0200

    Revert2

--- src/particle_filter/particelle.py
+++ src/particle_filter/particelle.py
@@ -31,20 +31,21 @@ class ParticleFilter:
         self.initialized = False
         rospy.init_node('Particle_Filter')
         self.base_frame = "base_link"  # the frame of the robot base
-        self.map_frame = "map"  # the name of the map coordinate
+        self.map_frame = "odom"  # the name of the map coordinate
         self.odom_frame = "odom"
         self.scan_topic = "player0/scan"
         self.n_particles = 1000
         self.particle_pub = rospy.Publisher("particlecloud", PoseArray, queue_size=10)
         self.tf_listener = TransformListener()
         self.particle_cloud = []
-        self.pose_listener = rospy.Subscriber("initialpose", PoseWithCovarianceStamped, self.update_initial_pose)
-        self.laser_subscriber = rospy.Subscriber(self.scan_topic, LaserScan, self.lidar_received)
+        #self.pose_listener = rospy.Subscriber("initialpose", PoseWithCovarianceStamped, self.update_initial_pose)
+        #self.laser_subscriber = rospy.Subscriber(self.scan_topic, LaserScan, self.lidar_received)
         self.current_odom_xy_theta = []
 
         self.initialized = True
 
     def publish_particles(self, msg):
+        print("ho pubblicato")
         particles_conv = []
         for p in self.particle_cloud:
             particles_conv.append(p.as_pose())
@@ -60,43 +61,16 @@ class ParticleFilter:
         self.initialize_particle_cloud(xy_theta)
         self.fix_map_to_odom_transform(msg)
 
-    def lidar_received(self, msg):
-        # listen to transform
-        (trans, rot) = self.tf_listener.lookupTransform(self.base_frame, self.odom_frame, rospy.Time(0))
-        # print the transform
-        rospy.loginfo('---------')
-        rospy.loginfo('Translation: ' + str(trans))
-        rospy.loginfo('Rotation: ' + str(rot))
 
-        self.odom_pose = trans, rot
-        # print(self.odom_pose)
-        new_odom_xy_theta = convert_pose_to_xy_and_theta(self.odom_pose)
-        self.update_initial_pose(new_odom_xy_theta)
-        ''' Sta roba a che cazzo servirà?
-        # calculate pose of laser relative ot the robot base
-        
-        p = PoseStamped(header=Header(stamp=rospy.Time.now(),
-                                      frame_id=msg.header.frame_id))
-
-
-        self.laser_pose = self.tf_listener.transformPose(self.base_frame, p)
-
-        # find out where the robot thinks it is based on its odometry
-        p = PoseStamped(header=Header(stamp=msg.header.stamp,
-                                      frame_id=self.base_frame),
-                        pose=Pose())
-       
-        self.odom_pose = self.tf_listener.transformPose(self.odom_frame, p)
-        # store the the odometry pose in a more convenient format (x,y,theta)
-        new_odom_xy_theta = convert_pose_to_xy_and_theta(self.odom_pose.pose)'''
 
     def initialize_particle_cloud(self, map, xy_theta=None):
-        if xy_theta is None:
-            xy_theta = convert_pose_to_xy_and_theta(self.odom_pose.pose)
+        print("diobestia mi sono sottoscritto")
+        #if xy_theta is None:
+           # xy_theta = convert_pose_to_xy_and_theta(self.odom_pose.pose)
         rad_min = map.info.width * map.info.resolution / 2  # meters
         rad_max = map.info.height * map.info.resolution / 2
         self.particle_cloud = []
-        self.particle_cloud.append(Particle(xy_theta[0], xy_theta[1], xy_theta[2]))  # origine robot ??
+        #self.particle_cloud.append(Particle(xy_theta[0], xy_theta[1], xy_theta[2]))  # origine robot ??
 
         while len(self.particle_cloud) < self.n_particles:
             # initial facing of the particle
@@ -119,15 +93,7 @@ class ParticleFilter:
 
         return self.particle_cloud
 
-    def fix_map_to_odom_transform(self, msg):
-        """ This method constantly updates the offset of the map and
-            odometry coordinate systems based on the latest results from
-            the localizer """
-        (translation, rotation) = convert_pose_inverse_transform(self.robot_pose)
-        p = PoseStamped(pose=convert_translation_rotation_to_pose(translation, rotation),
-                        header=Header(stamp=msg.header.stamp, frame_id=self.base_frame))
-        self.odom_to_map = self.tf_listener.transformPose(self.odom_frame, p)
-        (self.translation, self.rotation) = convert_pose_inverse_transform(self.odom_to_map.pose)
+
 
 
 
@@ -136,7 +102,6 @@ if __name__ == '__main__':
     try:
         pf = ParticleFilter()
 
-        time.sleep(0.3)
         rospy.Subscriber("/projected_map", OccupancyGrid, pf.initialize_particle_cloud)
         # rospy.Subscriber("/projected_map", OccupancyGrid, callback)
         rospy.spin()
