8e1ff0a91d24dbbb9934117913f790f982940833
lorentz995 <lorenzonaticchi@yahoo.it>
2021-04-21 15:58:04 +0200

    Revert

--- src/develop/connect.py
+++ src/develop/connect.py
@@ -14,7 +14,7 @@ def start_node():
 	message.ports = [9734]
 	message.json_settings = ['{ "sensor_settings": { "CarEncoder": {}, "RGBCamera": { "width": 240, "height": 240, "channels": "RGB", "FOV" : 90, "show": false }, "Laser": { "start_angle_x": -135.0, "end_angle_x": 135.0, "distance_angle_x": 0.2, "start_angle_y": 0.0, "end_angle_y": 0.0, "distance_angle_y": 1.0, "laser_range": 4000.0, "render": -1 }, "GPS": {}, "Lidar": { "start_angle_x": -45.0, "end_angle_x": 45.0, "distance_angle_x": 0.1, "start_angle_y": 0.0, "end_angle_y": 45.0, "distance_angle_y": 2, "lidar_range": 3000.0 , "render": -1} }, "action_manager_settings": { "ContinuousActionManager": { "command_dict": { "TURN": 0, "STRAIGHT": 1 }, "settings": { } } }, "reset_manager_settings": { "TrackerResetManager": {} }, "observations_step": ["Laser", "GPS", "RGBCamera", "Lidar"], "observations_reset": ["RGBCamera"] }']
 	message.render = True
-	message.address = '172.21.80.1'
+	message.address = '172.22.192.1'
 	resp = connect_service(message)
 	print(resp)
 
