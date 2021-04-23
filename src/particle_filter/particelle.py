7b2e2dfdb35600949e2b9b774f482afe03f7b134
lorentz995 <lorenzonaticchi@yahoo.it>
2021-04-23 12:20:19 +0200

    Added measurement model (don't work)

--- src/particle_filter/particelle.py
+++ src/particle_filter/particelle.py
@@ -7,7 +7,7 @@ from std_msgs.msg import Header, String, ColorRGBA
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped, PoseArray, Pose, Point, Quaternion, Vector3
 from nav_msgs.msg import Odometry
-
+import sys
 from visualization_msgs.msg import Marker, MarkerArray
 from copy import deepcopy
 
@@ -41,6 +41,14 @@ class ParticleFilter:
         self.motion_variance_x = 0.05
         self.motion_variance_y = 0.025
         self.motion_variance_theta = 0.25
+
+        self.lidar_angles_x = None
+        self.lidar_angles_y = None
+        self.tiled_angles = None
+        self.first_sensor_update = False
+
+        self.queries = None
+        self.ranges = None
         # Publishers
         self.particle_pub = rospy.Publisher("particlecloud", PoseArray, queue_size=10)
 
@@ -49,10 +57,10 @@ class ParticleFilter:
         self.odom_sub = rospy.Subscriber("player0/gps/odom", Odometry, self.odom)
         self.particle_cloud = []
 
-        # self.current_odom_xy_theta = []
-
         self.local_deltas = np.zeros((self.n_particles, 3))
 
+        # Weights
+        self.weights = np.ones(self.n_particles) / float(self.n_particles)
         self.initialized = True
 
     def publish_particles(self, msg):
@@ -109,6 +117,39 @@ class ParticleFilter:
         proposal_dist[:, 1] += np.random.normal(loc=0.0, scale=self.motion_variance_y, size=self.n_particles)
         proposal_dist[:, 2] += np.random.normal(loc=0.0, scale=self.motion_variance_theta, size=self.n_particles)
 
+    def measurement_model(self, proposal_dist, num_rays, obs, weights, map):
+        # TODO: che cazzo è num_rays
+        self.lidar_angles_x = np.linspace(-45, 45, 91)
+        self.lidar_angles_y = np.linspace(0, 45, 46)
+        # self.downsampled_angles = np.copy(self.laser_angles[0::self.ANGLE_STEP]).astype(np.float32)
+
+        if self.first_sensor_update:
+            self.queries = np.zeros((self.n_particles, 3), dtype=np.float32)
+            self.ranges = np.zeros(num_rays * self.n_particles, dtype=np.float32)
+            self.tiled_angles = np.tile(self.lidar_angles_x, self.n_particles)
+            self.first_sensor_update = False
+
+        self.queries[:, 0] = np.repeat(proposal_dist[:, 0], num_rays)
+        self.queries[:, 1] = np.repeat(proposal_dist[:, 1], num_rays)
+        self.queries[:, 2] = np.repeat(proposal_dist[:, 2], num_rays)
+        self.queries[:, 2] += self.tiled_angles
+
+        # compute the ranges for all the particles in a single functon call
+
+
+        # resolve the sensor model by discretizing and indexing into the precomputed table
+        obs /= float(map.info.resolution)
+        ranges = self.ranges / float(map.info.resolution)
+
+        intobs = np.rint(obs).astype(np.uint16)
+        intrng = np.rint(ranges).astype(np.uint16)
+
+        # compute the weight for each particle
+        for i in range(self.n_particles):
+            weight = np.product(self.sensor_model_table[intobs, intrng[i * num_rays:(i + 1) * num_rays]])
+            weight = np.power(weight, self.INV_SQUASH_FACTOR)
+            weights[i] = weight
+
     def odom(self, msg):
         position = np.array([msg.pose.pose.position.x, msg.pose.pose.position.y])
 
@@ -116,6 +157,7 @@ class ParticleFilter:
         pose = np.array([position[0], position[1], orientation])
 
         print(pose)
+
 
 if __name__ == '__main__':
     try:
